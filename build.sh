74a2b163fa4d0487288d0967697f84ac5c8009e5
Nicholas Bollweg <nbollweg@continuum.io>
2016-04-06 12:42:05 -0400

    what ends up in etc?

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 "${PYTHON}" -m nb_conda_kernels.install --enable --prefix "${PREFIX}"
 "${PYTHON}" -m nb_conda.setup install --enable --prefix "${PREFIX}"
 "${PYTHON}" -m nbpresent.install --overwrite --enable --prefix="${PREFIX}"
+ls "${PREFIX}/etc/jupyter"
