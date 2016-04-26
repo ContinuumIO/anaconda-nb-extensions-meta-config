b3540393c8fcacaa4cb13af90741a9f5cb370532
Nicholas Bollweg <nbollweg@continuum.io>
2016-04-26 15:03:32 -0400

    updating for notebook 4.2, 32b

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-"${PREFIX}/bin/jupyter-nbextension" enable nb_conda --py --sys-prefix --overwrite
 "${PREFIX}/bin/jupyter-nbextension" enable nb_anacondacloud --py --sys-prefix --overwrite
+"${PREFIX}/bin/jupyter-nbextension" enable nb_conda --py --sys-prefix --overwrite
 "${PREFIX}/bin/jupyter-nbextension" enable nbpresent --py --sys-prefix --overwrite
+
+"${PREFIX}/bin/jupyter-serverextension" enable nb_anacondacloud --py --sys-prefix --overwrite
+"${PREFIX}/bin/jupyter-serverextension" enable nb_conda --py --sys-prefix --overwrite
+"${PREFIX}/bin/jupyter-serverextension" enable nbpresent --py --sys-prefix --overwrite
+
 "${PYTHON}" -m nb_conda_kernels.install --enable --prefix "${PREFIX}"
