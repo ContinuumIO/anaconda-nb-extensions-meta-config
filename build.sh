3529256a5a27ce04c9fe56ade2939c00c1a399a6
Nicholas Bollweg <nbollweg@continuum.io>
2016-04-26 15:07:01 -0400

    nb, not server

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-"${PREFIX}/bin/jupyter-nbextension" enable nb_anacondacloud --py --sys-prefix "${PREFIX}/bin/jupyter-nbextension" enable nb_conda --py --sys-prefix "${PREFIX}/bin/jupyter-nbextension" enable nbpresent --py --sys-prefix 
+"${PREFIX}/bin/jupyter-nbextension" enable nb_anacondacloud --py --sys-prefix
+"${PREFIX}/bin/jupyter-nbextension" enable nb_conda --py --sys-prefix
+"${PREFIX}/bin/jupyter-nbextension" enable nbpresent --py --sys-prefix
+
 "${PREFIX}/bin/jupyter-serverextension" enable nb_anacondacloud --py --sys-prefix
-"${PREFIX}/bin/jupyter-serverextension" enable nb_conda --py --sys-prefix --overwrite
-"${PREFIX}/bin/jupyter-serverextension" enable nbpresent --py --sys-prefix --overwrite
+"${PREFIX}/bin/jupyter-serverextension" enable nb_conda --py --sys-prefix
+"${PREFIX}/bin/jupyter-serverextension" enable nbpresent --py --sys-prefix
 
 "${PYTHON}" -m nb_conda_kernels.install --enable --prefix "${PREFIX}"
